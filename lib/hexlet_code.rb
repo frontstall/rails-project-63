b94dd9f3364d1b90fee2e300b5fbd90990ad2d5f
Roman Makarov <deepspiral@yandex.ru>
2024-10-15 19:18:46 +0300

    add form_for

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -7,4 +7,14 @@ module HexletCode
   class Error < StandardError; end
 
   HexletCode.autoload :Tag, "./lib/hexlet_code/tag"
+
+  class << self
+    def form_for(_entity, attrs = {})
+      action = attrs[:url]
+      action ||= "#"
+      method = attrs[:method]
+      method ||= "post"
+      self::Tag.build "form", action:, method:
+    end
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -6,4 +6,16 @@ class TestHexletCode < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
+
+  def test_form_has_default_attrs
+    user = Struct.new :name, keyword_init: true
+    form = HexletCode.form_for user
+    assert { form == "<form action=\"#\" method=\"post\"></form>" }
+  end
+
+  def test_form_has_action
+    user = Struct.new :name, keyword_init: true
+    form = HexletCode.form_for user, url: "/users"
+    assert { form == "<form action=\"/users\" method=\"post\"></form>" }
+  end
 end
